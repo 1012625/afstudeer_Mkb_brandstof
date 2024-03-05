1079407cd4b205d0bde559a13cc8830f05df8b3d
Mohamed-1012625 <1012625@hr.nl>
2024-03-05 11:52:12 +0100

    afstudeer opdr

---
+++ src/components/Homepage.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css"; // Import the CSS file
+import mkbFotoImage from '../mkb-foto.png';
+import laadpaalImage from '../laadpaal.png'; // Import the laadpaal.png image
+
+const Homepage = () => {
+  const navigate = useNavigate();
+  const [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    return storedDarkMode ? JSON.parse(storedDarkMode) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const handleLoginButtonClick = () => {
+    console.log("login button clicked");
+    navigate("/Login");
+  };
+
+  const handlehome = () => {
+    console.log("login button clicked");
+    navigate("/");
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+
+  return (
+    <div className={`homepage-container ${darkMode ? 'dark-mode' : ''}`} style={{ paddingTop: '400px' }}>
+      <div className={`taskbar ${darkMode ? 'dark-mode-taskbar' : ''}`}>
+        <button className="homepage-button" onClick={handleLoginButtonClick}>
+          Klant login
+        </button>
+        <button className="dark-mode-toggle" onClick={toggleDarkMode}>
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+      </div>
+      <button className="mkb-brandstof-button" onClick={handlehome}>
+        mkb.
+      </button>
+      <button className="mkb-brandstof-button2" onClick={handlehome}>
+        brandstof
+      </button>
+      <p className="welcome-text">
+        Welkom op je verbruiksdashboard van MKB Brandstof!
+      </p>
+      <p className="welcome-text">
+        Hiermee krijg je binnen een handomdraai slimme inzichten over je verbruik. Log hier rechtsboven in met je Online Dashboard gegevens.
+      </p>
+      <img src={mkbFotoImage} alt="mkb" className="mkb-fotoImage" style={{ width: 'auto', height: 'auto' }} />
+      <p className="laadpaal-text">Laad je elektrische voertuig op met gemak bij MKB Brandstof! Wij bieden niet alleen een tankpas,</p>
+      <p className="laadpaal-text">maar ook de mogelijkheid tot het installeren van laadpalen.</p>
+      <img src={laadpaalImage} alt="laadpaal" className="laadpaalImage" style={{ width: '550px', height: 'auto' }} />
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/components/Overview.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
+import fuelImage from '../fuel.png'; 
+import smokeImage from '../smoke.png';
+
+function Overview() {
+  const navigate = useNavigate();
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/overview');
+      console.log("Response from server:", response.data); // Log response data
+      setQuestions(response.data);
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+    }
+  };
+
+  const handleHomeButtonClick = () => {
+    console.log("Home button clicked");
+    navigate("/");
+  };
+
+  return (
+    <div className="feedback-overview-container">
+      <div className="taskbar">
+        <img src={fuelImage} alt="fuel" className="fuel-image" />
+      </div>
+      <div className="content-below-taskbar">
+        <button className="mkb-brandstof-button" onClick={handleHomeButtonClick}>
+          mkb.
+        </button>
+        <button className="mkb-brandstof-button2" onClick={handleHomeButtonClick}>
+          brandstof
+        </button>
+        <div className="feedback-overview center-content">
+          {questions.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            questions.map((question) => (
+              <div key={question.id} className="feedback-overview-item">
+                <div className="feedback-overview-item-rectangle">
+                  <span className="feedback-overview-item-question">
+                    Hier is het totale aantal liters dat je hebt getankt sinds je lid bent geworden van MKB-Brandstof: {question.liters} 🚗💨
+                  </span>
+                </div>
+                <div className="feedback-overview-item-rectangle2">
+                  <span className="feedback-overview-item-question2">
+                    Laat ons eens een kijkje nemen onder de motorkap: de brandstof waar jij op vertrouwt is: {question.type} 🛣️💨
+                  </span>
+                </div>
+                <div className="feedback-overview-item-rectangle3">
+                  <span className="feedback-overview-item-question3">
+                    Jouw CO2-uitstoot sinds je bij MKB-Brandstof bent ongeveer: {calculateCO2(question.liters, question.type)} gram ☁️🌫️
+                  </span>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      <img src={smokeImage} alt="smoke" className="smoke-image" />
+    </div>
+  );
+}
+
+function calculateCO2(liters, type) {
+  // Hier kun je de CO2-uitstoot berekenen op basis van het type brandstof en het aantal liters
+  // Vervang de onderstaande dummy berekening door de werkelijke berekening
+  if (type === "benzine"){
+    const co2PerLiter = 2.4;
+    return liters * co2PerLiter;
+  }
+  else if (type === "diesel"){
+    const co2PerLiter = 2.64;
+    return liters * co2PerLiter;
+  }
+}
+
+
+export default Overview;
