e4d77705f2e1ee9805abcac3bd60a5703f2260f1
Mohamed-1012625 <1012625@hr.nl>
2024-03-07 15:30:16 +0100

    added styling

--- src/components/choose.js
+++ src/components/choose.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import fuel2Image from '../fuel2.png';
 import "../App.css";
 
 const Choose = () => {
@@ -14,12 +15,22 @@ const Choose = () => {
     console.log("overview button clicked");
     navigate("/overview");
   };
+  const handleinfo2 = () => {
+    console.log("overview button clicked");
+    navigate("/overview");
+  };
 
   return (
     <div className={`homepage-container`}>
       <div className={`taskbar`}>
+        <img src={fuel2Image} alt="fuel2" className="fuel-image" />
+
         <button className="overview-button" onClick={handleinfo}>
-          overview
+          Inzichten
+        </button>
+
+        <button className="overview-button2" onClick={handleinfo2}>
+          Gebruikspatronen
         </button>
       </div>
       <button className="mkb-brandstof-button" onClick={handlehome}>
